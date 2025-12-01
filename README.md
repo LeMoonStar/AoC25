04fa7728461b4ab7e807193dd3ae9b1142bef33c
LeMoonStar <me@lemoonstar.dev>
2025-12-01 17:43:06 +0100

    init Based on my Advent Of Code 2024 Framework

---
+++ README.md
@@ -0,0 +1,37 @@
+# Advent of Code 2025
+
+[![About](https://img.shields.io/badge/Advent%20of%20Code-2025-brightgreen?style=flat-square)](https://adventofcode.com/2025/about)
+[![Language: Rust](https://img.shields.io/badge/Language-Rust-orange.svg?style=flat-square)](https://en.wikipedia.org/wiki/Rust_(programming_language))
+[![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg?style=flat-square)](https://mit-license.org/)
+![Days completed](https://img.shields.io/badge/Days%20completed-0-red?style=flat-square)
+![Stars](https://img.shields.io/badge/Stars-0-yellow?style=flat-square)
+
+My Advent of Code 2025 Solutions.
+
+## Usage
+
+There are multiple ways to run my solutions, the easiest and most comfortable one is the `auto` command:  
+It automatically downloads your input. For this it requires you to provide your Advent of Code session id, which you can find in the websites cookies after logging in.  
+Simply provide the session token by setting the `AOC_SESSION` environment variable or using the -s argument:  
+`AOC_SESSION=XXXMYSESSION ./aoc25 [DAY] auto` or `./aoc25 [DAY] auto -s XXXMYSESSION`.  
+In this example, the environment variable for the AoC session is set using `export AOC_SESSION=XXXMYSESSION`, so I can run the command without specifying the session token again:  
+![auto command in action](./images/auto.png)  
+
+If you don't want to automatically download the input, you can also use the `run` command, which uses a locally stored file or the stdin input:  
+`./aoc25 [DAY] run -f my_input.txt`:  
+![run command in action](./images/run.png)  
+
+If you just want to run the day's example, simply use the `test` command, as this project already includes the examples:
+`./aoc25 [DAY] test`:  
+![test command in action](./images/test.png)  
+
+## Compiling
+
+This project uses `Cargo`, so compiling is pretty easy:  
+`cargo build --release`  
+The resulting binary can be found at `./targets/release/aoc25`. You can also directly run the project using `cargo run --release [arguments for aoc25]`  
+the `--release` option is not required, but it results in better performance.
+
+## Check out other AoC25 solutions
+
+> TODO
---
+++ src/days/utils.rs
@@ -0,0 +1,162 @@
+/// A general purpose struct able to store 2-Dimensional maps of Tiles.
+///
+/// Can be parsed from a multi-line &str, if the T type implements [`From<char>`](std::convert::From).
+#[derive(Debug, Clone, Hash)]
+pub struct Map<T> {
+    tiles: Vec<Vec<T>>,
+    dim: (usize, usize),
+}
+
+#[allow(dead_code)]
+impl<T> Map<T> {
+    /// Returns the dimensions of the map as a tuple `(x: usize, y: usize)`
+    pub fn dimensions(&self) -> (usize, usize) {
+        self.dim
+    }
+
+    /// Gets the tile of a specified position.
+    ///
+    /// May return None, if the position is outside the dimensions.
+    pub fn get(&self, x: usize, y: usize) -> Option<&T> {
+        self.tiles.get(y)?.get(x)
+    }
+
+    /// Sets the tile of a specified position.
+    pub fn set(&mut self, x: usize, y: usize, value: T) {
+        self.tiles[y][x] = value;
+    }
+
+    /// Get the internally stored tile data.
+    pub fn get_raw_tiles(&self) -> &Vec<Vec<T>> {
+        &self.tiles
+    }
+
+    /// Get the internally stored tile data as mutable reference.
+    pub fn get_raw_tiles_mut(&mut self) -> &mut Vec<Vec<T>> {
+        &mut self.tiles
+    }
+}
+
+impl<T> From<Vec<Vec<T>>> for Map<T> {
+    /// Creates a Map from a `Vec<Vec<T>>`.
+    /// The dimensions are derived from the length of the y-Axis and first x line.
+    fn from(value: Vec<Vec<T>>) -> Self {
+        Self {
+            dim: (value[0].len(), value.len()),
+            tiles: value,
+        }
+    }
+}
+
+impl<T> From<&str> for Map<T>
+where
+    T: From<char>,
+{
+    fn from(value: &str) -> Self {
+        let tiles: Vec<Vec<T>> = value
+            .lines()
+            .map(|v| v.chars().map(|v| v.into()).collect())
+            .collect();
+
+        Self {
+            dim: (tiles[0].len(), tiles.len()),
+            tiles,
+        }
+    }
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum Direction {
+    North,
+    East,
+    South,
+    West,
+}
+
+#[allow(dead_code)]
+impl Direction {
+    pub fn opposite(&self) -> Direction {
+        match self {
+            Self::North => Self::South,
+            Self::South => Self::North,
+            Self::West => Self::East,
+            Self::East => Self::West,
+        }
+    }
+
+    pub fn walk_pos(
+        &self,
+        pos: (usize, usize),
+        upper_limit: (usize, usize),
+    ) -> Option<(usize, usize)> {
+        match self {
+            Self::North => {
+                if pos.1 != 0 {
+                    Some((pos.0, pos.1 - 1))
+                } else {
+                    None
+                }
+            }
+            Self::South => {
+                if pos.1 < upper_limit.1 {
+                    Some((pos.0, pos.1 + 1))
+                } else {
+                    None
+                }
+            }
+            Self::West => {
+                if pos.0 != 0 {
+                    Some((pos.0 - 1, pos.1))
+                } else {
+                    None
+                }
+            }
+            Self::East => {
+                if pos.0 < upper_limit.0 {
+                    Some((pos.0 + 1, pos.1))
+                } else {
+                    None
+                }
+            }
+        }
+    }
+
+    pub fn walk_pos_signed(
+        &self,
+        pos: (isize, isize),
+        lower_limit: (isize, isize),
+        upper_limit: (isize, isize),
+    ) -> Option<(isize, isize)> {
+        match self {
+            Self::North => {
+                if pos.1 > lower_limit.1 {
+                    Some((pos.0, pos.1 - 1))
+                } else {
+                    None
+                }
+            }
+            Self::South => {
+                if pos.1 < upper_limit.1 {
+                    Some((pos.0, pos.1 + 1))
+                } else {
+                    None
+                }
+            }
+            Self::West => {
+                if pos.0 > lower_limit.0 {
+                    Some((pos.0 - 1, pos.1))
+                } else {
+                    None
+                }
+            }
+            Self::East => {
+                if pos.0 < upper_limit.0 {
+                    Some((pos.0 + 1, pos.1))
+                } else {
+                    None
+                }
+            }
+        }
+    }
+}
