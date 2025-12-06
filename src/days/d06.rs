eea260e81fe1d99a7141b4a26fccdba563811398
LeMoonStar <me@lemoonstar.dev>
2025-12-06 19:14:57 +0100

    ✨ Day 6 Part 2

--- src/days/d06.rs
+++ src/days/d06.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use crate::dprintln;
 
 use super::{Answer, Day, DayImpl};
@@ -36,13 +38,48 @@ struct HomeworkCalculation {
 }
 
 impl HomeworkCalculation {
-    pub fn normal_from_collumn(value: &[String]) -> Self {
+    pub fn normal_from_column(value: &[String]) -> Self {
+        dprintln!("normal_from_column: {:?}", value);
         Self {
             numbers: value[..value.len() - 1]
                 .iter()
-                .map(|v| v.parse().unwrap())
+                .map(|v| v.trim().parse().unwrap())
                 .collect(),
-            operation: Operation::from(value.last().unwrap().as_str()),
+            operation: Operation::from(value.last().unwrap().as_str().trim()),
+        }
+    }
+
+    pub fn cephalopod_from_column(value: &[String]) -> Self {
+        dprintln!("cephalopod_from_column: {:?}", value);
+        let mut numbers: HashMap<usize, usize> =
+            HashMap::with_capacity(value.first().unwrap().len());
+
+        value.iter().enumerate().for_each(|(line_num, line)| {
+            line.chars().enumerate().for_each(|(char_num, char)| {
+                if !char.is_digit(10) {
+                    return;
+                }
+                let mut num = *numbers.get(&char_num).unwrap_or(&0);
+
+                dprintln!(
+                    "num {:?} line_num {:?} char_num {:?} char {:?}",
+                    num,
+                    line_num,
+                    char_num,
+                    char
+                );
+
+                num = num * 10 + char.to_digit(10).unwrap() as usize;
+
+                numbers.insert(char_num, num);
+            })
+        });
+
+        dprintln!("cephalopod_from_column result: {:?}\n\n", numbers);
+
+        Self {
+            numbers: numbers.into_iter().map(|(_, value)| value).collect(),
+            operation: Operation::from(value.last().unwrap().as_str().trim()),
         }
     }
 
@@ -53,20 +90,41 @@ impl HomeworkCalculation {
 
 #[derive(Debug, Clone)]
 pub struct HomeworkSheet {
-    collumns: Vec<Vec<String>>,
+    columns: Vec<Vec<String>>,
 }
 
 impl From<&str> for HomeworkSheet {
     fn from(value: &str) -> Self {
-        let split_lines = value
-            .lines()
-            .map(|line| line.split_whitespace().collect::<Vec<_>>())
-            .collect::<Vec<_>>();
+        let last_line = value.lines().last().unwrap();
+        let column_starts: Vec<usize> = last_line
+            .match_indices(|c: char| !c.is_whitespace())
+            .map(|(i, _)| i)
+            .collect();
+
+        dprintln!("Column starts: {:?}", column_starts);
+
+        let mut split_lines: Vec<Vec<String>> = Vec::with_capacity(value.lines().count());
+        for line in value.lines() {
+            let mut split_line = Vec::with_capacity(column_starts.len());
+
+            dprintln!("LINE {:?}", line);
+
+            for i in 0..column_starts.len() {
+                let start = column_starts[i];
+                let end = column_starts.get(i + 1).copied().unwrap_or(line.len());
+
+                split_line.push(line[start..end.min(line.len())].to_owned());
+            }
+
+            split_lines.push(split_line)
+        }
+
+        dprintln!("{:?}", split_lines);
 
         let num_cols = split_lines.first().unwrap().len();
 
         Self {
-            collumns: (0..num_cols)
+            columns: (0..num_cols)
                 .map(|i| split_lines.iter().map(|line| line[i].to_owned()).collect())
                 .collect(),
         }
@@ -75,14 +133,28 @@ impl From<&str> for HomeworkSheet {
 
 impl HomeworkSheet {
     fn get_normal_homework_calculations(&self) -> Vec<HomeworkCalculation> {
-        self.collumns
+        self.columns
             .iter()
-            .map(|v| HomeworkCalculation::normal_from_collumn(&v))
+            .map(|v| HomeworkCalculation::normal_from_column(&v))
             .collect()
     }
 
-    fn get_results(&self) -> Vec<usize> {
+    fn get_cephalopod_homework_calculations(&self) -> Vec<HomeworkCalculation> {
+        self.columns
+            .iter()
+            .map(|v| HomeworkCalculation::cephalopod_from_column(&v))
+            .collect()
+    }
+
+    fn get_normal_results(&self) -> Vec<usize> {
         self.get_normal_homework_calculations()
+            .iter()
+            .map(|v| v.get_result())
+            .collect()
+    }
+
+    fn get_cephalopod_results(&self) -> Vec<usize> {
+        self.get_cephalopod_homework_calculations()
             .iter()
             .map(|v| v.get_result())
             .collect()
@@ -104,10 +176,10 @@ impl DayImpl<Data> for Day<CURRENT_DAY> {
     }
 
     fn one(&self, data: &mut Data) -> Answer {
-        Answer::Number(data.get_results().into_iter().sum::<usize>() as u64)
+        Answer::Number(data.get_normal_results().into_iter().sum::<usize>() as u64)
     }
 
     fn two(&self, data: &mut Data) -> Answer {
-        Answer::Number(0 as u64)
+        Answer::Number(data.get_cephalopod_results().into_iter().sum::<usize>() as u64)
     }
 }
